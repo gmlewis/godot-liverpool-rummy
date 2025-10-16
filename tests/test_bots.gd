547d34c7cb47ef589798baaf93ad53e62c647307
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-10-16 13:17:20 -0400

    Add round 7 bot tests
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- tests/test_bots.gd
+++ tests/test_bots.gd
@@ -550,6 +550,36 @@ var bot_test_scenarios = [
 			'recommended_discards': ["JOKER-2-5"],
 		},
 	},
+	{
+		'name': 'Basic 3 Runs Meld Round 7 - NO DISCARD',
+		'round': 7,
+		'cards': ["4-hearts-0", "5-hearts-0", "6-hearts-0", "7-hearts-0", "4-clubs-0", "5-clubs-0", "6-clubs-0", "7-clubs-0", "4-spades-0", "5-spades-0", "6-spades-0", "7-spades-0"],
+		'want_evaluation': {
+			'can_be_personally_melded': [
+				{"type": "run", "card_keys": ["4-clubs-0", "5-clubs-0", "6-clubs-0", "7-clubs-0"]},
+				{"type": "run", "card_keys": ["4-hearts-0", "5-hearts-0", "6-hearts-0", "7-hearts-0"]},
+				{"type": "run", "card_keys": ["4-spades-0", "5-spades-0", "6-spades-0", "7-spades-0"]},
+			],
+			'can_be_publicly_melded': [],
+			'is_winning_hand': true,
+			'recommended_discards': [],
+		},
+	},
+	{
+		'name': 'All Jokers Meld Round 7 - NO DISCARD',
+		'round': 7,
+		'cards': ["JOKER-1-0", "JOKER-2-0", "JOKER-1-1", "JOKER-2-1", "JOKER-1-2", "JOKER-2-2", "JOKER-1-3", "JOKER-2-3", "JOKER-1-4", "JOKER-2-4", "JOKER-1-5", "JOKER-2-5", "JOKER-1-6"],
+		'want_evaluation': {
+			'can_be_personally_melded': [
+				{"type": "run", "card_keys": ["JOKER-1-6", "JOKER-1-0", "JOKER-2-0", "JOKER-1-1", "JOKER-2-1"]},
+				{"type": "run", "card_keys": ["JOKER-1-2", "JOKER-2-2", "JOKER-1-3", "JOKER-2-3"]},
+				{"type": "run", "card_keys": ["JOKER-1-4", "JOKER-2-4", "JOKER-1-5", "JOKER-2-5"]},
+			],
+			'can_be_publicly_melded': [],
+			'is_winning_hand': true,
+			'recommended_discards': [],
+		},
+	},
 ]
 
 func test_bot_scenarios() -> bool:
