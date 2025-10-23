da194594afd39f4b78941f71ac8f5866cda6947e
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-10-23 19:58:24 -0400

    Reset sparkle shaders when a player wins a round
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- state_machine/08-player_won_round_state.gd
+++ state_machine/08-player_won_round_state.gd
@@ -3,6 +3,10 @@ extends GameState
 
 func enter(_params: Dictionary):
 	Global.dbg("ENTER PlayerWonRoundState")
+	# Clear all sparkle shaders (set to fuzzy) for all 3 meld areas
+	Global.emit_meld_area_state_changed_signal(false, 0)
+	Global.emit_meld_area_state_changed_signal(false, 1)
+	Global.emit_meld_area_state_changed_signal(false, 2)
 	Global.send_animate_winning_confetti_explosion_signal(5000) # Trigger confetti explosion for 5 seconds
 
 func exit():
