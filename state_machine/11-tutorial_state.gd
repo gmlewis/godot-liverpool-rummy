a56abbd1dfa2935456daa5985ad09bc67f09b5af
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-10-26 21:58:20 -0400

    Write steps in the tutorial
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- state_machine/11-tutorial_state.gd
+++ state_machine/11-tutorial_state.gd
@@ -78,4 +78,16 @@ var steps = [
 		'en': 'Each round has a specific set of requirements for melding that must be met for a player to win that round. The exact number of books (groups or sets of 3 or more cards of the same rank) and runs (sequences of 4 or more cards in the same suit) required to win each round is displayed on each round’s screen at the top. Note that in rounds 1-6, players must meet the round’s melding requirements and personally meld their own hand before they can meld on other players’ melds. Round 7 is the only round where players must meld their own hand and have no cards remaining in order to win the round.',
 		'de': '',
 	},
+	{
+		'en': 'The game area is divided into two main sections: the top section of the screen displays all players’ public melds, while the bottom section is private to the local player and cards can be moved at any time and arranged in any way the player desires. To the left of the bottom section are meld areas where players can drag and drop cards to build up their own melds. Cards that have been moved to one of the meld areas can not be accidentally tapped on which would otherwise cause them to be discarded during the player’s turn. Cards in the lower right area, however, can be tapped on to discard them during the player’s turn.',
+		'de': '',
+	},
+	{
+		'en': 'A player’s turn is indicated by a pulsing orange border around their player icon in the upper section of the screen. On a player’s turn, they must first draw a card either from the stock pile on the left or from the discard pile on the right by tapping on the desired pile. Once a player has completely satisfied the round’s melding requirements by dragging cards into the meld areas such that they are all sparkling, a "Meld!" indicator will appear on the player’s icon in the top section of the screen. The player can then tap on this "Meld!" indicator to meld their hand. After melding, the player may then drag cards from their private hand area into the meld areas on the left (whether for books or runs) and then publicly meld these cards when the "Meld!" indicator appears on any player icon in the top section (including possibly their own). Finally, for rounds 1-6, the player must discard a card by tapping on it in the lower right area to complete their turn. If the player has no cards remaining after discarding, then they have won the round. Note that in round 7, the first person to meld must have no cards remaining, and therefore they do not meld on other players’ melds (since they were the first to meld).',
+		'de': '',
+	},
+	{
+		'en': 'After each round, the host clicks the "Tally Scores" button where all players’ cards are totaled and counted against their scores. The host then clicks "Next Round" (or "Final Scores" after round 7). After all 7 rounds have been played, the player with the lowest score wins the game! A final scoreboard is displayed showing all players’ scores for each round as well as their total scores along with 3rd, 2nd, and 1st place trophies being awarded. After this, each player can click "Main Menu" to return to the main menu and start a new game if desired.',
+		'de': '',
+	},
 ]
